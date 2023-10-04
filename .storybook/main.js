e3568855de18ba59f6d9c66277d6e9ce3dd195a2
Fernanda Rodriguez <fer@Fernandas-MacBook-Pro.local>
2023-10-04 15:12:36 -0500

    chroe:implementacion componente button

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,6 +4,10 @@ export default {
   // Replace your-framework with the framework you are using (e.g., react-vite, vue3-vite)
   framework: '@storybook/react-vite',
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    // Other Storybook addons
+    '@storybook/addon-interactions', // 👈 Register the addon
+  ],
   async viteFinal(config, { configType }) {
     if (configType === 'DEVELOPMENT') {
       // Your development configuration goes here
