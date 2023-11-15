b47388d4a0e62c9751d6806fa87f94729427c80a
Fernanda Rodriguez <fer@Fernandas-MacBook-Pro.local>
2023-11-15 13:59:24 -0500

    logica y factorizacion de codigo

--- src/Context/index.jsx
+++ src/Context/index.jsx
@@ -1,44 +1,14 @@
 import React, {useEffect} from 'react'
 import {useState} from 'react'
 import useLocalStorage from './useLocalStorage'
-import _default from '@storybook/addon-onboarding'
 
 const TrickyContext = React.createContext()
 
 const TrickyProvider = ({children}) => {
-  const [statePlayers, setStatePlayers] = React.useState([
+  const {statePlayers, getPlayerSelect, selecPlayer} = useLocalStorage([
     {value: 'x', selected: true},
     {value: 'o', selected: false},
   ])
-
-  const {upDatePlayers, setUpDatePlayers} = useLocalStorage(statePlayers)
-
-  const selecPlayer = player => {
-    const indexPlayer = statePlayers.findIndex(p => p.value === player)
-    const updatedState = statePlayers.map(p => {
-      return {
-        ...p,
-        selected: false,
-      }
-    })
-
-    updatedState[indexPlayer].selected = true
-    setStatePlayers(updatedState)
-    setUpDatePlayers(updatedState)
-  }
-
-  const getPlayerSelect = () => {
-    return statePlayers
-      .filter(p => {
-        return p.selected
-      })[0]
-      .value.toUpperCase()
-  }
-  useEffect(() => {
-    if (upDatePlayers) {
-      setStatePlayers(upDatePlayers)
-    }
-  }, [])
 
   return (
     <TrickyContext.Provider
@@ -46,7 +16,6 @@ const TrickyProvider = ({children}) => {
         statePlayers,
         selecPlayer,
         getPlayerSelect,
-        setUpDatePlayers,
       }}
     >
       {children}
--- src/Context/useLocalStorage.jsx
+++ src/Context/useLocalStorage.jsx
@@ -3,13 +3,34 @@ import React, {useState, useEffect} from 'react'
 const useLocalStorage = initialPlayers => {
   const storedPlayers =
     JSON.parse(localStorage.getItem('players')) || initialPlayers
-  const [upDatePlayers, setUpDatePlayers] = useState(storedPlayers)
+  const [statePlayers, setStatePlayers] = useState(storedPlayers)
+
+  const selecPlayer = player => {
+    const indexPlayer = statePlayers.findIndex(p => p.value === player)
+    const updatedState = statePlayers.map(p => {
+      return {
+        ...p,
+        selected: false,
+      }
+    })
+
+    updatedState[indexPlayer].selected = true
+    setStatePlayers(updatedState)
+  }
+
+  const getPlayerSelect = () => {
+    return statePlayers
+      .filter(p => {
+        return p.selected
+      })[0]
+      .value.toUpperCase()
+  }
 
   useEffect(() => {
-    localStorage.setItem('players', JSON.stringify(upDatePlayers))
-  }, [upDatePlayers])
+    localStorage.setItem('players', JSON.stringify(statePlayers))
+  }, [statePlayers])
 
-  return {upDatePlayers, setUpDatePlayers}
+  return {statePlayers, getPlayerSelect, selecPlayer}
 }
 
 export default useLocalStorage
