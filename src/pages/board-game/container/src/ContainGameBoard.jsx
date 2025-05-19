f4eabd7bf87747249386afbfbce81019cba3ad0d
Fernanda Rodriguez <fer@Fernandas-MacBook-Pro.local>
2025-05-19 10:03:08 -0500

    ajustes de reduxtoolkit

--- src/pages/board-game/container/src/ContainGameBoard.jsx
+++ src/pages/board-game/container/src/ContainGameBoard.jsx
@@ -19,25 +19,9 @@ import ReportGame from '../../../../common/components/reportGame/src/ReportGame'
 import ModalReststart from '../../../../common/components/modal-reststart/src/ModalReststart'
 import ModalTied from '../../../../common/components/modal-tied/src/ModalTied'
 import {useSelector} from 'react-redux'
-const ContainGameBoard = ({
-  type,
-  label,
-  textReport,
-  takeRound,
-  value,
-  onClick,
-}) => {
-  const initialBoard = [
-    {p: '0,0', value: '', index: '0', selected: false, winner: false},
-    {p: '0,1', value: '', index: '1', selected: false, winner: false},
-    {p: '0,2', value: '', index: '2', selected: false, winner: false},
-    {p: '1,0', value: '', index: '3', selected: false, winner: false},
-    {p: '1,1', value: '', index: '4', selected: false, winner: false},
-    {p: '1,2', value: '', index: '5', selected: false, winner: false},
-    {p: '2,0', value: '', index: '6', selected: false, winner: false},
-    {p: '2,1', value: '', index: '7', selected: false, winner: false},
-    {p: '2,2', value: '', index: '8', selected: false, winner: false},
-  ]
+const ContainGameBoard = ({textReport, takeRound, value, onClick}) => {
+  const initialBoard = useSelector(state => state.board)
+
   const currentPlayer = useSelector(state =>
     state.players.find(player => player.selected),
   )
--- src/redux/board/slice.jsx
+++ src/redux/board/slice.jsx
@@ -3,12 +3,12 @@ import {createSlice} from '@reduxjs/toolkit'
 export const boardSlice = createSlice({
   name: 'board',
   initialState: [
-    {p: '0,0', value: 'x', index: '0', selected: true, winner: false},
+    {p: '0,0', value: '', index: '0', selected: false, winner: false},
     {p: '0,1', value: '', index: '1', selected: false, winner: false},
     {p: '0,2', value: '', index: '2', selected: false, winner: false},
     {p: '1,0', value: '', index: '3', selected: false, winner: false},
     {p: '1,1', value: '', index: '4', selected: false, winner: false},
-    {p: '1,2', value: '', index: '5', selected: false, winner: false},
+    {p: '1,2', value: 'o', index: '5', selected: false, winner: false},
     {p: '2,0', value: '', index: '6', selected: false, winner: false},
     {p: '2,1', value: '', index: '7', selected: false, winner: false},
     {p: '2,2', value: '', index: '8', selected: false, winner: false},
